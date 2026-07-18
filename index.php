6f360c3665cf7a6fb3c911e057e52f371c9c6727
aptsamuel <vibesames0@gmail.com>
2026-07-18 01:26:47 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Invoicent - Main Root Router
- * Instantly routes incoming domain visitors to the secure login module.
- */
-
-// Route the browser to the login page using a standard 302 redirect
-header("Location: auth/login.html");
-exit;
